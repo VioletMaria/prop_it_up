ff02d14cd390e85ed2af56f27ab90aad59517235
Violetta Szewczyk <szewczykviolet@gmail.com>
2022-01-13 08:04:14 -0600

    state commit

---
+++ src/components/PersonCard.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class PersonCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            ageCounter: this.props.age
+        }
+    }
+
+    increaseAge = () => {
+        this.setState({ageCounter: this.state.ageCounter + 1})
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: {this.state.ageCounter}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick = {this.increaseAge}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
+            </div>
+        );
+    }
+}
+
+export default PersonCard;
